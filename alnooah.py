56cb99bc9aff1956b69f0d14b3eaa4cb931a86c3
ayman alnooah <79175683+aymanalnooah@users.noreply.github.com>
2023-07-23 01:48:42 +0300

    alnooah.py

---
+++ alnooah.py
@@ -0,0 +1,16 @@
+ /ip firewall address-list
+add address=157.240.227.0/24 comment="Whatsapp" list=LAN2024
+add address=23.90.159.38 list=0TIKTOK comment=free
+add address=202.81.112.209 list=0TIKTOK comment=free
+
+
+add address=71.18.1.229 comment="free fire" list=0TIKTOK
+add address=71.18.1.236 comment="free fire" list=0TIKTOK
+remove [find address=157.240.227.61]
+remove [find address=157.240.227.1]
+remove [find address=34.104.35.84]
+remove [find address=219.93.26.46]
+
+
+
+
