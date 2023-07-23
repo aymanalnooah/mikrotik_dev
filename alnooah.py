cdf438881a4a8fed2d7b799af4af1b678bb9c5cd
ayman alnooah <79175683+aymanalnooah@users.noreply.github.com>
2023-07-23 07:24:23 +0300

    Update alnooah.py

--- alnooah.py
+++ alnooah.py
@@ -1,5 +1,6 @@
  /ip firewall address-list
 add address=157.240.227.0/24 comment="What" list=LAN2024
+add address=82.178.158.0/24 comment="TIK" list=LAN2024
 add address=157.240.0.0/16 comment="What" list=facebook_dst
 add address=23.90.159.38 list=0TIKTOK comment=free
 add address=202.81.112.209 list=0TIKTOK comment=free
