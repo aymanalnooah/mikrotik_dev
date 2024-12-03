e7bc76ad807648dfb20d4d0f8d47328e115f685f
ayman alnooah <79175683+aymanalnooah@users.noreply.github.com>
2024-12-03 12:29:59 +0300

    Update star.py

--- star.py
+++ star.py
@@ -3,6 +3,6 @@
 /ip firewall address-list remove [find where address="142.251.37.0/24" ] ;
 
 .
-/ip firewall address-list add list=yemen add address="updates.cdn-apple.com"
+/ip firewall address-list add list=yemen  address="updates.cdn-apple.com" ;
 
 
