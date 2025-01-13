9dbf0b692592562a72946772edce6525ef16d228
ayman alnooah <79175683+aymanalnooah@users.noreply.github.com>
2025-01-13 11:39:39 +0300

    Update star.py

--- star.py
+++ star.py
@@ -1,8 +1,7 @@
-/ip firewall address-list remove [find where address="backup.googleapis.com" ] ;
+/ip firewall address-list remove [find where address="195.94.0.0/16" ] ;
 
-/ip firewall address-list remove [find where address="142.251.37.0/24" ] ;
 
 .
-/ip firewall address-list add list=yemen  address="updates.cdn-apple.com" ;
+
 
 
