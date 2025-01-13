dbba1880bf9817f38b7953362a8951525377b481
ayman alnooah <79175683+aymanalnooah@users.noreply.github.com>
2025-01-13 10:02:57 +0300

    Update hotspot.py

--- hotspot.py
+++ hotspot.py
@@ -1,101 +1,18 @@
 
-:delay 10 ;
-
-/ip firewall address-list ; 
-
-remove [find where list="Whats-face" and dynamic=no ] ; 
-
-add address=connectivitycheck.gstatic.com list=Whats-face
-add address=msftconnecttest.com list=Whats-face
-add address=dns.msftncsi.com list=Whats-face
-add address=captive.apple.com list=Whats-face
-add address=13.107.4.50 list=Whats-face
-add address=17.0.0.0/8 list=Whats-face
-add address=91.189.89.199 comment="Ubuntu Connectivity Check" list=Whats-face
-add address=192.12.31.105 list=Whats-face
-add address=142.251.0.0/16 comment=TEST disabled=yes list=Whats-face
-add address=36.103.232.0/24 comment=TEST list=Whats-face
-
-add address=221.204.16.0/24 comment=TEST disabled=yes list=Whats-face
-add address=216.239.38.0/24 comment=TEST-google disabled=yes list=Whats-face
-add address=124.163.195.0/24 comment=TEST list=Whats-fac
-add address=124.163.195.0/24 list=Whats-face
-add address=36.103.232.0/24 list=Whats-face
-add address=192.12.31.105 list=Whats-face
-
-
-add address=g.whatsapp.net comment=Whatsapp list=Whats-face
-add address=dit.whatsapp.net comment=Whatsapp list=Whats-face
-add address=31.13.88.61 comment=Whatsapp list=Whats-face
-add address=static.whatsapp.net comment=Whatsapp list=Whats-face
-add address=157.240.22.54 comment=Whatsapp list=Whats-face
-add address=157.240.19.54 comment=Whatsapp list=Whats-face
-add address=31.13.66.51 comment=Whatsapp list=Whats-face
-add address=31.13.67.53 comment=Whatsapp list=Whats-face
-add address=31.13.93.54 comment=Whatsapp list=Whats-face
-add address=31.13.71.50 comment=Whatsapp list=Whats-face
-add address=3.33.221.48 comment=Whatsapp list=Whats-face
-add address=15.197.206.217 comment=Whatsapp list=Whats-face
-add address=157.240.3.55 comment=Whatsapp list=Whats-face
-add address=31.13.70.50 comment=Whatsapp list=Whats-face
-add address=3.33.252.61 comment=Whatsapp list=Whats-face
-add address=15.197.210.208 comment=Whatsapp list=Whats-face
-add address=157.240.11.54 comment=Whatsapp list=Whats-face
-add address=157.240.241.61 comment=Whatsapp list=Whats-face
-add address=31.13.65.50 comment=Whatsapp list=Whats-face
-add address=157.240.14.53 comment=Whatsapp list=Whats-face
-add address=157.240.229.61 comment=Whatsapp list=Whats-face
-add address=157.240.18.53 comment=Whatsapp list=Whats-face
-add address=255.0.0.0/8 comment=Whatsapp list=Whats-face
-add address=185.11.11.32 comment=Whatsapp list=Whats-face
 
 
 
 
+/ip hotspot walled-garden
+add dst-host=*tvquran.com*
 
 
+add dst-host=*quran.tv*
 
 
+/ip firewall address-list
 
-add address=hotspot.alnooah.pro list=Whats-face
-add address=hotspot.alnooah.pro list=hotspot.alnooah
-add address=10.0.0.0/8 list=Whats-face
-add address=192.0.0.0/8 list=Whats-face
-add address=20.20.20.20 list=Whats-face
-add address=1.0.0.0/8 list=Whats-face
-add address=8.8.8.8 list=Whats-face
-add address=8.8.4.4 list=Whats-face
-add address=157.240.0.0/16 list=Whats-face
-add address=172.0.0.0/8 list=Whats-face
-add address=127.0.0.0/8 list=Whats-face
-add address=1.0.0.0/24 list=Whats-face
-add address=1.1.1.0/24 list=Whats-face
-
-
-
-
-add address=10.0.0.1 list=Whats-face
-add address=172.0.0.0/8 list=Whats-face
-add address=57.144.148.0/24 comment=test list=Whats-face
-add comment=test list=Whats-face
-add address=255.255.255.255 comment=test list=Whats-face
-add address=255.0.0.0/8 comment=test list=Whats-face
-
-
-add address=10.0.0.0/8 list=LAN2024
-add address=192.0.0.0/8 list=LAN2024
-add address=20.20.20.20 list=LAN2024
-add address=1.0.0.0/8 list=LAN2024
-add address=8.8.8.8 list=LAN2024
-add address=8.8.4.4 list=LAN2024
-add address=157.240.0.0/16 list=LAN2024
-add address=172.0.0.0/8 list=LAN2024
-add address=127.0.0.0/8 list=LAN2024
-add address=1.0.0.1 list=LAN2024
-add address=1.1.1.1 list=LAN2024
-add address=255.0.0.0/8 list=LAN2024
-add address=hotspot.alnooah.pro list=LAN2024
-add address=hotspot.alnooah.pro list=hotspot.alnooah
-
-
-
+add address=quran.tv list=hotspot.alnooah
+add address=tvquran.com list=hotspot.alnooah
+add address=quran.tv list=tvquran.com
+add address=tvquran.com list=tvquran.com
