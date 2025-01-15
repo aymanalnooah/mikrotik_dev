21f17db226b8f499e7a795c9a8795185aa80a1b2
ayman alnooah <79175683+aymanalnooah@users.noreply.github.com>
2025-01-15 14:02:57 +0300

    Update hotspot.py

--- hotspot.py
+++ hotspot.py
@@ -1,20 +1,2 @@
 
 
-
-
-
-/ip hotspot walled-garden
-add dst-host=*tvquran.com*
-
-
-add dst-host=*quran.tv*
-
-
-/ip firewall address-list
-
-add address=quran.tv list=hotspot.alnooah
-add address=tvquran.com list=hotspot.alnooah
-add address=quran.tv list=tvquran.com
-add address=tvquran.com list=tvquran.com
-
-
