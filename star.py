2bd63bc42a7968374e5c3ac77f0713bf109ac02b
ayman alnooah <79175683+aymanalnooah@users.noreply.github.com>
2025-01-19 22:12:04 +0300

    Update star.py

--- star.py
+++ star.py
@@ -2,3 +2,54 @@
 
 
 .
+/ip firewall address-list
+add list=yemen address=5.100.160.0/21
+add list=yemen address=31.31.176.0/20
+add list=yemen address=38.56.24.0/21
+add list=yemen address=46.35.64.0/19
+add list=yemen address=46.161.224.0/19
+add list=yemen address=63.168.168.0/23
+add list=yemen address=63.171.18.0/23
+add list=yemen address=63.173.172.0/23
+add list=yemen address=65.162.184.0/23
+add list=yemen address=78.137.64.0/19
+add list=yemen address=80.253.176.0/20
+add list=yemen address=81.91.24.0/21
+add list=yemen address=82.114.160.0/19
+add list=yemen address=89.189.64.0/19
+add list=yemen address=94.26.192.0/19
+add list=yemen address=109.74.32.0/20
+add list=yemen address=109.200.160.0/19
+add list=yemen address=110.238.32.0/19
+add list=yemen address=131.117.160.0/21
+add list=yemen address=134.35.0.0/16
+add list=yemen address=145.224.125.0/24
+add list=yemen address=145.224.127.0/24
+add list=yemen address=157.167.77.0/24
+add list=yemen address=185.11.124.0/22
+add list=yemen address=185.71.128.0/22
+add list=yemen address=185.76.0.0/22f
+add list=yemen address=185.94.64.0/22
+add list=yemen address=185.240.72.0/22
+add list=yemen address=185.240.76.0/22
+add list=yemen address=196.45.128.0/19
+add list=yemen address=196.45.160.0/19
+add list=yemen address=196.45.192.0/19
+add list=yemen address=196.62.0.0/19
+add list=yemen address=196.205.128.0/19
+add list=yemen address=196.216.64.0/19
+add list=yemen address=197.251.192.0/19
+add list=yemen address=197.252.0.0/19
+add list=yemen address=197.253.64.0/19
+add list=yemen address=197.253.128.0/19
+add list=yemen address=197.254.0.0/19
+add list=yemen address=197.254.64.0/19
+add list=yemen address=197.254.128.0/19
+add list=yemen address=197.255.0.0/19
+add list=yemen address=197.255.64.0/19
+add list=yemen address=197.255.128.0/19
+add list=yemen address=197.255.192.0/19
+add list=yemen address=195.94.0.0/16
+add list=yemen address=212.26.0.0/19
+
+/ip firewall address-list add list=yemen  address="updates.cdn-apple.com" ;
