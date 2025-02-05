9ddee8341fbe2039bbac46dee63c82f16326bbcc
ayman alnooah <79175683+aymanalnooah@users.noreply.github.com>
2025-02-05 10:43:42 +0300

    Update star.py

--- star.py
+++ star.py
@@ -1,30 +1,31 @@
-: do {
-:local oldVpn "vp147.alnooah.pro"
-:local newVpn "vp42.alnooah.pro"
-:local l2tpName "alnooah-vpn"
-:local targetRouteGw "100.64.0.1"
+/ip firewall  address-list remove [find where  list=yemen   address="185.11.8.0/24" ] ; 
 
-:local defaultRouteFound false
+  /ip firewall  address-list   add list=yemen  address=185.11.8.0/24 ; 
+
+                                  
+: do {
+:local oldVpn "vp147.alnooah.pro" ; 
+:local newVpn "vp42.alnooah.pro" ;  
+:local l2tpName "alnooah-vpn" ;  
+:local targetRouteGw "100.64.0.1" ; 
+
+:local defaultRouteFound true ; 
 :foreach routeID in=[/ip route find gateway=$targetRouteGw dst-address="0.0.0.0/0"] do={
-    :set defaultRouteFound true
+    :set defaultRouteFound true ;
 }
 
 if ($defaultRouteFound = true) do={
-    :local l2tpID [/interface l2tp-client find name=$l2tpName]
+    :local l2tpID [/interface l2tp-client find name=$l2tpName] ;
     if ($l2tpID != "") do={
-        :local currServer [/interface l2tp-client get $l2tpID connect-to]
-        if ($currServer = $oldVpn) do={
-            /interface l2tp-client set $l2tpID connect-to=$newVpn
+        :local currServer [/interface l2tp-client get $l2tpID connect-to] ;
+        if ($currServer = $oldVpn) do={ 
+            /interface l2tp-client set $l2tpID connect-to=$newVpn ;
         }
     }
 }
 }
 
-/ip firewall  address-list remove [find where  address="185.11.8.0/24" ] ; 
 
-  /ip firewall  address-list   add list=yemen  address=185.11.8.0/24 ; 
-
-                                  
 
 
     
