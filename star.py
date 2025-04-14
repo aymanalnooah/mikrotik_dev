d7f280e1158fc336199dbadedc532abd22dbc1ac
ayman alnooah <79175683+aymanalnooah@users.noreply.github.com>
2025-04-14 05:26:59 +0300

    Update star.py

--- star.py
+++ star.py
@@ -1,5 +1,15 @@
 
-/ip firewall address-list
+/ip firewall address-list ;
 
-add address=185.11.8.0/24 list=yemen ; 
+
+
+
+ remove   [ find where  address=196.216.64.0/19 ] ;  
+
+  add address=185.11.8.0/24 list=yemen ;  
+
+
+  add address=5.255.16.0/24 list=yemen ;  
+
+  add address=185.240.64.0/22 list=yemen ;  
 
