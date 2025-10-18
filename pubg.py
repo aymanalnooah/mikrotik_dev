003a2ed76dcc2fa567a8885e9a76182ea88892bc
ayman alnooah <79175683+aymanalnooah@users.noreply.github.com>
2025-10-18 17:39:49 +0300

    Update pubg.py

--- pubg.py
+++ pubg.py
@@ -1,5 +1,5 @@
-/ip firewall address-list add address=litedev.sgp.hik-connect.com comment="كيمرات هيك فيجين" list=game ; 
+/ip firewall address-list add address=litedev.sgp.hik-connect.com comment="camera" list=game ; 
 
-/ip firewall address-list add address=8.214.27.0/24 comment="كيمرات هيك فيجين" list=game ; 
+/ip firewall address-list add address=8.214.27.0/24 comment="camera" list=game ; 
 
 
